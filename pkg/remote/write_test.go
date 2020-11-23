44ae2fc451da628930bb4c61eebea46bfdb74111
Christian Simon <simon@swine.de>
2020-11-23 11:13:23 +0000

    Add testing of all cases during remote write
    Signed-off-by: Christian Simon <simon@swine.de>

--- pkg/remote/write_test.go
+++ pkg/remote/write_test.go
@@ -2,14 +2,17 @@ package remote
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"math"
+	"sync"
 	"testing"
 	"time"
 
 	"github.com/golang/snappy"
 	"github.com/prometheus/common/model"
 	"github.com/prometheus/prometheus/prompb"
+	"github.com/prometheus/prometheus/storage/remote"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
@@ -132,50 +135,17 @@ func newSampleInf() pulsar.ReceivedSample {
 	return s
 }
 
-func TestWriter_Run_BatchSize_NoTenantID(t *testing.T) {
+type runTestCase struct {
+	name   string
+	send   func(chan pulsar.ReceivedSample)
+	verify func(testing.TB, *fakeWriteClient)
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	samplesCh := make(chan pulsar.ReceivedSample)
-
-	w := NewWrite()
-	wc := newFakeWriteClient(t)
-
-	// reduce size to test
-	w.checkInterval = time.Microsecond
-	w.BatchSize = 2
-	// avoid hitting that
-	w.BatchMaxDelay = time.Hour
-
-	// watch channel
-	go func() {
-		err := w.Run(ctx, samplesCh, wc)
-		require.NoError(t, err)
-	}()
-
-	// send two samples without channel ID
-	samplesCh <- newSampleNormal()
-	samplesCh <- newSampleInf()
-
-	// retrieve request
-	reqW := <-wc.reqCh
-	wc.errCh <- nil
-
-	req := reqW.req
-
-	require.Equal(t, 2, len(req.Timeseries))
-
-	// first sample
-	assert.Equal(t, labelsPrompb, req.Timeseries[0].Labels)
-	assert.Equal(t, []prompb.Sample{sampleNormal}, req.Timeseries[0].Samples)
-
-	// second sample
-	assert.Equal(t, labelsPrompb, req.Timeseries[1].Labels)
-	assert.Equal(t, []prompb.Sample{sampleInf}, req.Timeseries[1].Samples)
+	batchMaxDelay time.Duration
+	batchSize     int
 }
 
-func TestWriter_Run_BatchSize_TenantIDs(t *testing.T) {
+func (tc runTestCase) test(t *testing.T) {
+	var wg sync.WaitGroup
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
@@ -184,46 +154,223 @@ func TestWriter_Run_BatchSize_TenantIDs(t *testing.T) {
 	w := NewWrite()
 	wc := newFakeWriteClient(t)
 
-	// reduce size to test
 	w.checkInterval = time.Microsecond
-	w.BatchSize = 2
-	// avoid hitting that
-	w.BatchMaxDelay = time.Hour
+	if tc.batchSize > 0 {
+		w.BatchSize = tc.batchSize
+
+	} else {
+		// reduce size to test
+		w.BatchSize = 2
+	}
+	if tc.batchMaxDelay > 0 {
+		w.BatchMaxDelay = tc.batchMaxDelay
+	} else {
+		// set very high value, to avoid hitting that in the default case
+		w.BatchMaxDelay = time.Hour
+	}
 
 	// watch channel
+	wg.Add(1)
 	go func() {
+		defer wg.Done()
 		err := w.Run(ctx, samplesCh, wc)
 		require.NoError(t, err)
 	}()
 
-	// send two samples per tenant ID
+	// send samples
+	wg.Add(1)
 	go func() {
-		samplesCh <- newSampleNormal()
-		samplesCh <- withTenantID(newSampleNormal(), "team-a")
-		samplesCh <- withTenantID(newSampleNormal(), "team-b")
-		samplesCh <- newSampleInf()
-		samplesCh <- withTenantID(newSampleInf(), "team-a")
-		samplesCh <- withTenantID(newSampleInf(), "team-b")
+		defer wg.Done()
+		tc.send(samplesCh)
 	}()
 
-	for _, tenantID := range []string{"", "team-a", "team-b"} {
+	// retrieve request
+	tc.verify(t, wc)
+
+	cancel()
+	wg.Wait()
+
+}
+
+func testForTenantIDs(t testing.TB, wc *fakeWriteClient, tenantIDSlice []string, verify func(testing.TB, *prompb.WriteRequest)) {
+	tenantIDs := make(map[string]struct{})
+	for _, tenantID := range tenantIDSlice {
+		tenantIDs[tenantID] = struct{}{}
+	}
+
+	for {
+		if len(tenantIDs) == 0 {
+			break
+		}
 		// retrieve request
 		reqW := <-wc.reqCh
 		wc.errCh <- nil
 
 		req := reqW.req
 
-		assert.Equal(t, tenantID, mcontext.TenantIDFromContext(reqW.ctx))
+		tenantID := mcontext.TenantIDFromContext(reqW.ctx)
+		if _, ok := tenantIDs[tenantID]; !ok {
+			t.Fatalf("tenant ID %s unexpected", tenantID)
+		}
+		delete(tenantIDs, tenantID)
 
-		require.Equal(t, 2, len(req.Timeseries))
+		verify(t, req)
+	}
+}
 
-		// first sample
-		assert.Equal(t, labelsPrompb, req.Timeseries[0].Labels)
-		assert.Equal(t, []prompb.Sample{sampleNormal}, req.Timeseries[0].Samples)
+func TestWriter_Run(t *testing.T) {
 
-		// second sample
-		assert.Equal(t, labelsPrompb, req.Timeseries[1].Labels)
-		assert.Equal(t, []prompb.Sample{sampleInf}, req.Timeseries[1].Samples)
+	for _, tc := range []runTestCase{
+		{
+			name: "send after reaching batchsize",
+			send: func(ch chan pulsar.ReceivedSample) {
+				// send two samples without channel ID
+				ch <- newSampleNormal()
+				ch <- newSampleInf()
+			},
+			verify: func(t testing.TB, wc *fakeWriteClient) {
+				reqW := <-wc.reqCh
+				wc.errCh <- nil
+
+				req := reqW.req
+
+				require.Equal(t, 2, len(req.Timeseries))
+
+				// first sample
+				assert.Equal(t, labelsPrompb, req.Timeseries[0].Labels)
+				assert.Equal(t, []prompb.Sample{sampleNormal}, req.Timeseries[0].Samples)
+
+				// second sample
+				assert.Equal(t, labelsPrompb, req.Timeseries[1].Labels)
+				assert.Equal(t, []prompb.Sample{sampleInf}, req.Timeseries[1].Samples)
+			},
+		},
+		{
+			name: "send after reaching batchsize with tenants",
+			send: func(ch chan pulsar.ReceivedSample) {
+				// send two samples per tenant ID
+				ch <- newSampleNormal()
+				ch <- withTenantID(newSampleNormal(), "team-a")
+				ch <- withTenantID(newSampleNormal(), "team-b")
+				ch <- newSampleInf()
+				ch <- withTenantID(newSampleInf(), "team-a")
+				ch <- withTenantID(newSampleInf(), "team-b")
+			},
+			verify: func(t testing.TB, wc *fakeWriteClient) {
+				testForTenantIDs(t, wc, []string{"", "team-a", "team-b"}, func(t testing.TB, req *prompb.WriteRequest) {
+					require.Equal(t, 2, len(req.Timeseries))
+
+					// first sample
+					assert.Equal(t, labelsPrompb, req.Timeseries[0].Labels)
+					assert.Equal(t, []prompb.Sample{sampleNormal}, req.Timeseries[0].Samples)
+
+					// second sample
+					assert.Equal(t, labelsPrompb, req.Timeseries[1].Labels)
+					assert.Equal(t, []prompb.Sample{sampleInf}, req.Timeseries[1].Samples)
+				})
+			},
+		},
+		{
+			name: "send after reaching max delay",
+			send: func(ch chan pulsar.ReceivedSample) {
+				// send a sample
+				ch <- newSampleNormal()
+			},
+			verify: func(t testing.TB, wc *fakeWriteClient) {
+				reqW := <-wc.reqCh
+				wc.errCh <- nil
+
+				req := reqW.req
+				require.Equal(t, 1, len(req.Timeseries))
+
+				// first sample
+				assert.Equal(t, labelsPrompb, req.Timeseries[0].Labels)
+				assert.Equal(t, []prompb.Sample{sampleNormal}, req.Timeseries[0].Samples)
+			},
+			batchMaxDelay: time.Microsecond,
+		},
+		{
+			name: "send after reaching max delay with tenants",
+			send: func(ch chan pulsar.ReceivedSample) {
+				// send sample per tenant ID
+				ch <- newSampleNormal()
+				ch <- withTenantID(newSampleNormal(), "team-a")
+				ch <- withTenantID(newSampleNormal(), "team-b")
+			},
+			verify: func(t testing.TB, wc *fakeWriteClient) {
+				testForTenantIDs(t, wc, []string{"", "team-a", "team-b"}, func(t testing.TB, req *prompb.WriteRequest) {
+					require.Equal(t, 1, len(req.Timeseries))
+
+					// first sample
+					assert.Equal(t, labelsPrompb, req.Timeseries[0].Labels)
+					assert.Equal(t, []prompb.Sample{sampleNormal}, req.Timeseries[0].Samples)
+				})
+			},
+			batchMaxDelay: time.Microsecond,
+		},
+		{
+			name: "send after recoverable error",
+			send: func(ch chan pulsar.ReceivedSample) {
+				// send sample
+				ch <- newSampleNormal()
+				ch <- newSampleInf()
+			},
+			verify: func(t testing.TB, wc *fakeWriteClient) {
+				// retrieve request but error
+				<-wc.reqCh
+				wc.errCh <- fmt.Errorf("I am very recoverable: %w", remote.RecoverableError{})
+
+				// receive the second time round
+				reqW := <-wc.reqCh
+				wc.errCh <- nil
+
+				req := reqW.req
+				require.Equal(t, 1, len(req.Timeseries))
+
+				// first sample
+				assert.Equal(t, labelsPrompb, req.Timeseries[0].Labels)
+				assert.Equal(t, []prompb.Sample{sampleNormal}, req.Timeseries[0].Samples)
+
+				reqW = <-wc.reqCh
+				wc.errCh <- nil
+
+				req = reqW.req
+				require.Equal(t, 1, len(req.Timeseries))
+
+				// second sample
+				assert.Equal(t, labelsPrompb, req.Timeseries[0].Labels)
+				assert.Equal(t, []prompb.Sample{sampleInf}, req.Timeseries[0].Samples)
+			},
+			batchSize: 1,
+		},
+		{
+			name: "send after unrecoverable error",
+			send: func(ch chan pulsar.ReceivedSample) {
+				// send sample
+				ch <- newSampleNormal()
+				ch <- newSampleInf()
+			},
+			verify: func(t testing.TB, wc *fakeWriteClient) {
+				// retrieve request but error
+				<-wc.reqCh
+				wc.errCh <- errors.New("I am very unrecoverable")
+
+				// receive the second time round
+				reqW := <-wc.reqCh
+				wc.errCh <- nil
+
+				req := reqW.req
+				require.Equal(t, 1, len(req.Timeseries))
+
+				// second sample only
+				assert.Equal(t, labelsPrompb, req.Timeseries[0].Labels)
+				assert.Equal(t, []prompb.Sample{sampleInf}, req.Timeseries[0].Samples)
+
+			},
+			batchSize: 1,
+		},
+	} {
+		t.Run(tc.name, tc.test)
 	}
 }
 
